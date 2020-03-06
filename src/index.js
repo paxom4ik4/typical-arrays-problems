924e7a82661740deac2c32d1a5088e0e7f3f5f74
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-03-06 22:27:26 +0300

    feat: change code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+let min = array[0];
+let max = array[0];
+let sum = array[0];
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    for(let i; i < array.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min;
+  }
+  
+  exports.max = function max (array) {
+    for(let i; i < array.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return min;
+  }
+  
+  exports.avg = function avg (array) {
+    for(let i; i < array.length; i++){
+        sum += arr[i];
+    } 
+    return sum/array.length;
+  }
